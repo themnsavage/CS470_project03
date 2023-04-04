4c671a3649033788fe6abbcb09d7f08a53c9d623
themnsavage <mnsavage@crimson.ua.edu>
2023-04-04 01:06:55 -0500

    feat(data manager): added json and kept other files(.txt) extraction

--- app/data_manager.py
+++ app/data_manager.py
@@ -1,17 +1,24 @@
 import copy
+import json
 class Data_Manager:
-    def __init__(self, file_path):
+    def __init__(self, file_path = '', json_file_path = ''):
         self._file_path = file_path
+        self._json_file_path = json_file_path
         self._data = {
             "capacity": None,
             "weights": [],
             "values": []
         }
-        self._extract_file_content()
+        
+        if file_path != '':
+            self._extract_file_content()
+        if json_file_path != '':
+            self._extract_json_file_content()
+            
         
     def _extract_file_content(self):
-        with open(self._file_path, "r") as data_file:
-            file_lines = data_file.readlines()
+        with open(self._file_path, "r") as file_data:
+            file_lines = file_data.readlines()
         
         for line in file_lines:
             if self._data["capacity"] == None:
@@ -21,9 +28,14 @@ class Data_Manager:
                 self._data["weights"].append(float(weight_and_value[0]))
                 self._data["values"].append(float(weight_and_value[1]))
     
+    def _extract_json_file_content(self):
+        with open(self._json_file_path) as json_file_data:
+            self._data = json.load(json_file_data)
+        
     def get_data(self):
         return copy.deepcopy(self._data)
     
-    def print_file_data(self):
-        file_data = f"File Path: {self._file_path}\nCapacity: {self._data['capacity']}\n Weights: {self._data['weights']}\nValues: {self._data['values']}"
+    def print_data(self):
+        path = self._file_path if self._file_path != '' else self._json_file_path
+        file_data = f"File Path: {path}\nCapacity: {self._data['capacity']}\nWeights: {self._data['weights']}\nValues: {self._data['values']}"
         print(file_data)
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from app.data_manager import Data_Manager
 
 def main():
-    file_path= input("Enter path to file: ")
-    manager = Data_Manager(file_path)
-    manager.print_file_data()
+    file_path = 'data/data01.txt'
+    json_file_path = 'data/data01.json'
+    
+    txt_manager = Data_Manager(file_path=file_path)
+    json_manager = Data_Manager(json_file_path=json_file_path)
+    
+    print("txt manager data:")
+    txt_manager.print_data()
+    print("json manager data:")
+    json_manager.print_data()
     
 if __name__ == "__main__":
     main()

933784eff922323f52e6515303cf85b944e667c2
themnsavage <mnsavage@crimson.ua.edu>
2023-04-08 01:59:22 -0500

    refactor(data generator): remove un-used methods

--- app/data_generator.py
+++ app/data_generator.py
@@ -9,94 +9,8 @@ class Data_Generator:
         self._max_single_value = max_single_value
         self._data = None
 
-    def _generate_random_solution(self, max_items):
-        solution_item_count = random.randint(1, max_items)
-        solution = {"weights": [], "values": [], "max_weight": None, "min_value": None}
-
-        for _ in range(solution_item_count):
-            item_weight = random.randint(1, self._max_single_weight)
-            item_value = random.randint(1, self._max_single_value)
-            if solution["max_weight"] is None and solution["min_value"] is None:
-                solution["max_weight"] = item_weight
-                solution["min_value"] = item_value
-            else:
-                solution["max_weight"] = (
-                    item_weight
-                    if item_weight > solution["max_weight"]
-                    else solution["max_weight"]
-                )
-                solution["min_value"] = (
-                    item_value
-                    if item_value < solution["min_value"]
-                    else solution["min_value"]
-                )
-
-            solution["weights"].append(item_weight)
-            solution["values"].append(item_value)
-
-        return solution
 
     def generate_single_data_set(self, max_items=80):
-        solution = self._generate_random_solution(max_items)
-        other_item_weights = []
-        other_item_values = []
-        other_item_count = max_items - len(solution["weights"])
-
-        for _ in range(other_item_count):
-            other_item_weight = random.randint(1, solution["max_weight"])
-            other_item_value = random.randint(1, solution["min_value"])
-            other_item_weights.append(other_item_weight)
-            other_item_values.append(other_item_value)
-
-        info_list = []
-        for index in range(len(other_item_weights)):
-            info = {
-                "weight": other_item_weights[index],
-                "value": other_item_values[index],
-                "bit": 0,
-            }
-            info_list.append(info)
-        for index in range(len(solution["weights"])):
-            info = {
-                "weight": solution["weights"][index],
-                "value": solution["values"][index],
-                "bit": 1,
-            }
-            info_list.append(info)
-        random.shuffle(info_list)
-
-        weights = []
-        values = []
-        solution_bitvector = []
-
-        for info in info_list:
-            weights.append(info["weight"])
-            values.append(info["value"])
-            solution_bitvector.append(info["bit"])
-
-        single_data = {
-            "capacity": sum(solution["weights"]),
-            "weights": weights,
-            "values": values,
-            "solution": {
-                "capacity": sum(solution["weights"]),
-                "items_used": solution_bitvector,
-                "max_value": sum(solution["values"]),
-            },
-        }
-        self._data = single_data
-        return single_data
-
-    def generate_multiple_data_set(self, max_items=80):
-        data = []
-        for problem_set_size in range(2, max_items):
-            single_data_set = self.generate_single_data_set(problem_set_size)
-            data.append(single_data_set)
-
-        self._data = {"data": data}
-        return {"data": data}
-
-    def generate_single_data_set_without_solution(self, max_items=80):
         weights = []
         values = []
 
@@ -112,7 +26,7 @@ class Data_Generator:
         self._data = {"capacity": capacity, "weights": weights, "values": values}
         return self._data
 
-    def generate_multiple_data_set_without_solution(self, max_items=80):
+    def generate_multiple_data_set(self, max_items=80):
         multiple_data_sets = {"data": []}
         for single_data_set_size in range(1, max_items + 1):
             single_data_set = self.generate_single_data_set_without_solution(

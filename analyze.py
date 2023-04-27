d85660b0e37def402726f40e2afde9a248f73dd2
themnsavage <mnsavage@crimson.ua.edu>
2023-04-27 17:43:03 -0500

    anaylze: remove data extraction

--- analyze.py
+++ analyze.py
@@ -4,8 +4,6 @@ from app.data_extractor import Data_Extractor
 
 def main():
     Algorithm_Analyzer().analyze_data_sets(max_items=900)
-    # Data_Extractor().convert_json(file_path="data/worst_accuracy02.json") # want to export data generated
-
 
 if __name__ == "__main__":
     main()

99ebd1540e2e1fc22807e1ad73bb348f17fdfa37
themnsavage <mnsavage@crimson.ua.edu>
2023-04-27 17:46:55 -0500

    feat(reduction): read and export to file to reduce 3sat to knapsack

--- reduction.py
+++ reduction.py
@@ -1,14 +1,9 @@
 from app.np_reducer import Np_Reducer
-
+from app.data_extractor import Data_Extractor
 def main():
-    three_sat_data = [
-        [1, 2, -3],
-        [-1, 2, -3]
-    ]
-    
-    reducer = Np_Reducer()
-    knapsack_data = reducer.three_sat_to_knapsack(three_sat_data)
-    print(knapsack_data)
+    three_sat_data = Data_Extractor().extract_three_sat_data()
+    knapsack_data_from_three_sat = Np_Reducer().three_sat_to_knapsack(three_sat_data)
+    Data_Extractor().convert_json(data=knapsack_data_from_three_sat, file_path="data/output_files/knapsack_data_from_3sat.txt")
 
 
 if __name__ == "__main__":

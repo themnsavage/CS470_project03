621db7647fd15c74e99965ad89c830a07bed5e59
themnsavage <mnsavage@crimson.ua.edu>
2023-04-27 22:49:24 -0500

    doc(reduction): comments

--- reduction.py
+++ reduction.py
@@ -1,13 +1,24 @@
 from app.np_reducer import Np_Reducer
 from app.data_extractor import Data_Extractor
+
+
 def main():
+    """
+    Description: allow user to reduce/map np problems to each other
+    """
     three_sat_data = Data_Extractor().extract_three_sat_data()
     knapsack_data_from_three_sat = Np_Reducer().three_sat_to_knapsack(three_sat_data)
-    Data_Extractor().convert_json(data=knapsack_data_from_three_sat, file_path="data/output_reduction_files/knapsack_data_from_3sat.txt")
-    
-    knapsack_data = Data_Extractor(file_path="data/input_reduction_files/reduce_knapsack_to_traveling_salesman.txt").get_data()
+    Data_Extractor().convert_json(
+        data=knapsack_data_from_three_sat,
+        file_path="data/output_reduction_files/knapsack_data_from_3sat.txt",
+    )
+
+    knapsack_data = Data_Extractor(
+        file_path="data/input_reduction_files/reduce_knapsack_to_traveling_salesman.txt"
+    ).get_data()
     traveling_salesman_data = Np_Reducer().knapsack_to_traveling_salesman(knapsack_data)
     Data_Extractor().export_traveling_salesman_data(traveling_salesman_data)
+
 
 if __name__ == "__main__":
     main()
